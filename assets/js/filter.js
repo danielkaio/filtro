791dcb817de3e15a1f3966b2731adec2a8ff27b6
daniel <dani.ajala@yahoo.com>
2021-07-18 01:04:34 -0300

    filtro de pesquisa

---
+++ assets/js/filter.js
@@ -0,0 +1,21 @@
+let campo = document.querySelector('#nome')
+let li = document.querySelectorAll("li")
+
+
+
+campo.addEventListener('input', () => {
+    let filtro = campo.value
+
+    for (let i = 0; li.length; i++) {
+        let item = li[i].textContent
+
+        if (item.indexOf(filtro) > -1) {
+            li[i].style.display = ""
+
+        } else {
+            li[i].style.display = "none"
+        }
+
+    }
+
+})
